abc0862649d343988e80f30be5c2cf8be63211b0
CoderEvoke <CodersEvoke72@gmail.com>
2024-01-18 14:38:41 +0500

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+ import React from 'react'
+ 
+ const Navigation = () => {
+   return (
+     <div>
+        <div className="logo">
+            img
+        </div>
+     </div>
+   )
+ }
+ 
+ export default Navigation
